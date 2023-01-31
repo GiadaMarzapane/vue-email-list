749b279e98ab4569f488f5a9b4730fc5602f7247
GiadaMarzapane <giada.ortesta@gmail.com>
2023-01-31 16:20:18 +0100

    Push esercizio completo

--- js/script.js
+++ js/script.js
@@ -3,7 +3,15 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            myEmailsArray: []
         }
-    }
+    },
+    mounted() {
+        for (let index = 1; index < 10; index++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((resp) => {    
+                    this.myEmailsArray.push(resp.data.response);
+                })
+        }
+        }
 }).mount('#app')
